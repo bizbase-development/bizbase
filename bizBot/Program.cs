8b6a230247a15f47b163035f21abf06593732fce
bizbase-development <77630389+bizbase-development@users.noreply.github.com>
2021-01-30 15:14:41 +0000

    Update Program.cs

--- bizBot/Program.cs
+++ bizBot/Program.cs
@@ -1,4 +1,4 @@
-﻿using bizbase;
+using bizbase;
 using ContractWrappers.Contracts.Bizbase;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
@@ -13,6 +13,7 @@ using Uniswap.Contracts.UniswapV2Router02;
 
 
 if (!Directory.Exists("files")) Directory.CreateDirectory("files");
+if (!Directory.Exists("webserver")) Directory.CreateDirectory("webserver");
 
 
 var configFile = $"files{Path.DirectorySeparatorChar}config.json";
@@ -24,7 +25,7 @@ var errorFile = $"files{Path.DirectorySeparatorChar}errlog.txt";
 var bzbFile = $"files{Path.DirectorySeparatorChar}bzbdat.json";
 
 
-if (!File.Exists(bzbFile))
+if (!File.Exists(bzbFile) || File.ReadAllText(bzbFile).Length < 1)
 	File.WriteAllText(bzbFile, JsonConvert.SerializeObject(new List<IntervalData>(), Formatting.Indented));
 var intervals = JsonConvert.DeserializeObject<List<IntervalData>>(File.ReadAllText(bzbFile));
 
@@ -35,7 +36,12 @@ async Task updatePostCount() {
 		var str = await response.Content.ReadAsStringAsync();
 		catalog = JsonConvert.DeserializeObject<List<Root>>(str);
 	}
-	var currentLastPostNumber = catalog.SelectMany(ct => ct.threads).First(th => th.replies != 0).last_replies.Last().no;
+	var currentLastPostNumber = catalog
+		.SelectMany(ct => ct.threads)
+		.Where(th => th.last_replies != null)
+		.SelectMany(th => th.last_replies)
+		.Max(r => r.no);
+		//.First(th => th.closed != 1 && th.replies != 0).last_replies.Last().no;
 	var currentPostCount = -1;
 	if (intervals.Count != 0 && (DateTime.UtcNow - intervals.Last().Time) < TimeSpan.FromHours(36))
 		if (!cfg.Debug_MinuteRun || (DateTime.UtcNow - intervals.Last().Time) < TimeSpan.FromSeconds(90))
@@ -43,12 +49,19 @@ async Task updatePostCount() {
 	intervals.Add(new IntervalData() {
 		Time = DateTime.Now, PostNumber = currentLastPostNumber, PostCount = currentPostCount
 	});
-	File.WriteAllText(bzbFile, JsonConvert.SerializeObject(intervals, Formatting.Indented));
+
+	//File.WriteAllText(bzbFile, JsonConvert.SerializeObject(intervals, Formatting.Indented));
+}
+void refreshJsonVolumeData() {
+	var labels = intervals.Skip(1).Select(iv => iv.Time.ToString("MMM-d"));
+	var data = intervals.Skip(1).Select(iv => iv.PostCount);
+	var json = new { labels = labels, data = data };
+	File.WriteAllText($"webserver{Path.DirectorySeparatorChar}volumedata.json", JsonConvert.SerializeObject(json));
 }
 
 while (true) {
 	try {
-		await updateContract(50);
+		//await updateContract(50);
 		DateTime targetTime;
 		if (cfg.Debug_MinuteRun) {
 			targetTime = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour, DateTime.UtcNow.Minute, cfg.RebaseHourUTC, 0);
@@ -64,17 +77,28 @@ while (true) {
 		await Task.Delay((int)timeToWait.TotalMilliseconds);
 		Console.WriteLine($"{DateTime.UtcNow.ToString("o")} updating postcount");
 		await updatePostCount();
-		if (intervals.Last().PostCount != -1) {
+		if (intervals.Last().PostCount > 0) {
 			Console.WriteLine($"{DateTime.UtcNow.ToString("o")} updating contract");
-			await updateContract(intervals.Last().PostCount);
+			var dat = await updateContract(intervals.Last().PostCount);
+			
+			intervals[intervals.Count - 1].PrePrice = dat.prePrice;
+			intervals[intervals.Count - 1].PostPrice = dat.postPrice;
+			intervals[intervals.Count - 1].TotalSupplyBefore = dat.totalSupplyBefore;
+			intervals[intervals.Count - 1].TotalSupplyAfter = dat.totalSupplyAfter;
+			intervals[intervals.Count - 1].SupplyDelta = dat.supplyDelta;
+
+			if (intervals.Last().PrePrice == 0m) { }
+			refreshJsonVolumeData();
 		}
+
+		File.WriteAllText(bzbFile, JsonConvert.SerializeObject(intervals, Formatting.Indented));
 	} catch (Exception e) {
 		File.AppendAllText(errorFile, Environment.NewLine + DateTime.UtcNow.ToLongTimeString() + " " + e.ToString() + Environment.NewLine);
 	}
 }
 
 
-async Task updateContract(int postCount) {
+async Task<(decimal prePrice, decimal postPrice, decimal totalSupplyBefore, decimal totalSupplyAfter, decimal supplyDelta)> updateContract(int postCount) {
 	var addresses = ETHAddressCollection.ByNetwork(cfg.Network);
 
 	var account = new Account(new string(cfg.PrivateKey.Reverse().ToArray()));
@@ -103,6 +127,13 @@ async Task updateContract(int postCount) {
 	supplyDelta /= cfg.DampeningFactor;
 
 	await serv.RebaseRequestAndWaitForReceiptAsync(supplyDelta.ToBigInt9d());
+
+	_amountsOut = await router.GetAmountsOutQueryAsync(
+		1m.ToBigInt9d(), wethDai_path
+	);
+	var pricePost = _amountsOut.Last().ToDec();
+	var totalSupplyPost = await serv.TotalSupplyQueryAsync().ToDec9d();
+	return (price, pricePost, totalSupply, totalSupplyPost, supplyDelta);
 }
 
 decimal CalculateSupplyDelta(decimal totalySupply, decimal price, decimal oneDollarTargetPostCount, decimal postCount) {
@@ -113,7 +144,15 @@ decimal CalculateSupplyDelta(decimal totalySupply, decimal price, decimal oneDol
 
 
 public class IntervalData {
-	public DateTime Time; public int PostCount; public int PostNumber;
+	public DateTime Time; 
+	public int PostCount; 
+	public int PostNumber;
+
+	public decimal PrePrice;
+	public decimal PostPrice;
+	public decimal TotalSupplyBefore;
+	public decimal TotalSupplyAfter;
+	public decimal SupplyDelta;
 }
 public class BizBaseData {
 	public List<IntervalData> Intervals = new List<IntervalData>();
